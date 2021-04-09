770019373528a4ddf5e7d9dd17dcc6769b2998b3
Ashley Finafrock <v-asho@microsoft.com>
2021-04-09 07:49:56 -0700

    Render badge class dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,10 +8,16 @@ export default class Counter extends Component {
   render() {
     return (
       <div>
-        <span className="badge badge-primary">{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
