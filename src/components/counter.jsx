f58b5b59514f444b411898ca55dd8d399e7a6bca
Ashley Finafrock <v-asho@microsoft.com>
2021-04-09 21:01:22 -0700

    Pass args to event handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,6 +6,11 @@ export default class Counter extends Component {
     tags: [],
   };
 
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
   renderTags() {
     if (this.state.tags.length === 0) return <p>'There are no tags'</p>;
 
@@ -18,12 +23,38 @@ export default class Counter extends Component {
     );
   }
 
+  formatCount = () => {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  };
+
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  //   doHandleIncrement = (product) => {
+  //     this.handleIncrement({ id: 1 });
+  //   };
+
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && "Please create a new tag"}
         {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 }
