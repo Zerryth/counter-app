327e5adac0ea7ee8f6542e888bdcacf8906fdd99
Ashley Finafrock <v-asho@microsoft.com>
2021-04-13 21:14:52 -0700

    Remove local state from Counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,40 +1,10 @@
 import React, { Component } from "react";
 
 export default class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-    tags: [],
-  };
-
-  //   constructor() {
-  //     super();
-  //     this.handleIncrement = this.handleIncrement.bind(this);
-  //   }
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>'There are no tags'</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
   formatCount = () => {
-    const { value: count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
-  //   doHandleIncrement = (product) => {
-  //     this.handleIncrement({ id: 1 });
-  //   };
 
   render() {
     console.log("props", this.props);
@@ -44,7 +14,7 @@ export default class Counter extends Component {
         {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -61,7 +31,7 @@ export default class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
